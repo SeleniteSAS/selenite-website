e8d7f32e782c42fedcd3f6d553a4bc0ac6e4442e
Pierre Guéroult <pierre.gueroult@univ-rouen.fr>
2024-11-14 21:07:32 +0100

    tools(env): add type validation for environment variables

---
+++ src/lib/env.ts
@@ -0,0 +1,16 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { clientEnvSchemas, serverEnvSchemas } from "@/schemas/env";
+
+export const env = createEnv({
+  server: serverEnvSchemas,
+  client: clientEnvSchemas,
+  runtimeEnv: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    AUTH_SECRET: process.env.AUTH_SECRET,
+    AUTH_TRUST_HOST: process.env.AUTH_TRUST_HOST,
+    NEXT_PUBLIC_ROOT_URL: process.env.NEXT_PUBLIC_ROOT_URL,
+    NEXT_PUBLIC_AUTH_URL: process.env.NEXT_PUBLIC_AUTH_URL,
+    NEXT_PUBLIC_DOWNLOAD_URL: process.env.NEXT_PUBLIC_DOWNLOAD_URL,
+    NEXT_PUBLIC_WIKI_URL: process.env.NEXT_PUBLIC_WIKI_URL,
+  },
+});
