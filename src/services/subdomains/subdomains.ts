dcf6b84cc0b2b73e74607c009667ea077140944c
Pierre Guéroult <pierre.gueroult@univ-rouen.fr>
2024-11-17 21:52:49 +0100

    fix(services): move services from class to functions

--- src/services/subdomains/subdomains.ts
+++ src/services/subdomains/subdomains.ts
@@ -1,54 +1,52 @@
 import { Subdomain } from "@/types/subdomain";
 import { env } from "@/lib/env";
 
-class SubdomainsService {
-  static subdomains: Subdomain[] = [
-    {
-      name: "Website",
-      subdomain: "",
-      slug: "website",
-    },
-    {
-      name: "Wiki of the game",
-      subdomain: "wiki",
-      slug: "wiki",
-    },
-    {
-      name: "Source",
-      subdomain: "download",
-      slug: "download",
-    },
-    {
-      name: "Website",
-      subdomain: "www",
-      slug: "website",
-    },
-    {
-      name: "Authenticator",
-      subdomain: "auth",
-      slug: "auth",
-    },
-    {
-      name: "Design",
-      subdomain: "weare",
-      slug: "design",
-    },
-  ];
+const subdomains: Subdomain[] = [
+  {
+    name: "Website",
+    subdomain: "",
+    slug: "website",
+  },
+  {
+    name: "Wiki of the game",
+    subdomain: "wiki",
+    slug: "wiki",
+  },
+  {
+    name: "Source",
+    subdomain: "download",
+    slug: "download",
+  },
+  {
+    name: "Website",
+    subdomain: "www",
+    slug: "website",
+  },
+  {
+    name: "Authenticator",
+    subdomain: "auth",
+    slug: "auth",
+  },
+  {
+    name: "Design",
+    subdomain: "weare",
+    slug: "design",
+  },
+];
 
-  static getSubdomains(): Subdomain[] {
-    return this.subdomains;
-  }
-
-  static buildSubdomainUrl(subdomain: Subdomain, path: string, url: string): URL {
-    return new URL(`/${subdomain.slug}${path}`, url);
-  }
-
-  static isValidSubdomain(subdomain: Subdomain, hostname: string): boolean {
-    const rootDomain: string = new URL(env.NEXT_PUBLIC_ROOT_URL).hostname;
-    return (
-      hostname === `${subdomain.subdomain}.${rootDomain}` || (hostname === rootDomain && subdomain.subdomain === "")
-    );
-  }
+export function getSubdomains(): Subdomain[] {
+  return subdomains;
 }
 
-export default SubdomainsService;
+export function buildSubdomainUrl(subdomain: Subdomain, path: string, url: string): URL {
+  return new URL(`/${subdomain.slug}${path}`, url);
+}
+
+export function isValidSubdomain(subdomain: Subdomain, hostname: string): boolean {
+  const rootDomain: string = new URL(env.NEXT_PUBLIC_ROOT_URL).hostname;
+  return (
+    hostname === `${subdomain.subdomain}.${rootDomain}` || (hostname === rootDomain && subdomain.subdomain === "")
+  );
+}
+
+export { subdomains };
