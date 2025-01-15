83f0843f8a6f9f28af0fe242758c8c6a5998be5e
Pierre Guéroult <pierre.gueroult@univ-rouen.fr>
2025-01-15 10:42:33 +0100

    fix(leva): update leva provider display to fix production error

--- src/components/common-leva-provider/common-leva-provider.tsx
+++ src/components/common-leva-provider/common-leva-provider.tsx
@@ -6,9 +6,10 @@ import { Leva } from "leva";
 type CommonLevaProviderProps = Readonly<{ children: ReactNode }>;
 
 export default function CommonLevaProvider({ children }: CommonLevaProviderProps): ReactNode {
+  const displayLeva: boolean = process.env.NODE_ENV === "development";
   return (
     <Fragment>
-      {process.env.NODE_ENV === "development" && <Leva />}
+      <Leva hidden={!displayLeva} />
       {children}
     </Fragment>
   );
