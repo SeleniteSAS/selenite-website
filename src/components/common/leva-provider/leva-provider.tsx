1691d0317243ed0cde29b84ea668dc0de29f34b9
Pierre Guéroult <pierre.gueroult@univ-rouen.fr>
2025-01-21 22:04:54 +0100

    feat(landing-page): create new landing page with animated 3d features

--- src/components/common/leva-provider/leva-provider.tsx
+++ src/components/common/leva-provider/leva-provider.tsx
@@ -3,12 +3,12 @@
 import { Leva } from "leva";
 import { type ReactNode, Fragment } from "react";
 
-type CommonLevaProviderProps = Readonly<{ children: ReactNode }>;
+type LevaProviderProps = Readonly<{ children: ReactNode }>;
 
-export default function LevaProvider({ children }: CommonLevaProviderProps) {
+export default function LevaProvider({ children }: LevaProviderProps): ReactNode {
   return (
     <Fragment>
-      <Leva hidden={process.env.NODE_ENV === "production"} />
+      <Leva hidden={process.env.NODE_ENV === "production"} collapsed={true} />
       {children}
     </Fragment>
   );
--- src/components/website/header/header.tsx
+++ src/components/website/header/header.tsx
@@ -1,29 +1,68 @@
+import { env } from "@/lib/env";
 import Link from "next/link";
+import { buttonVariants } from "@/components/_ui/button";
+import { cn } from "@/lib/utils";
+
+const links = [
+  {
+    label: "Home",
+    url: "/",
+  },
+  {
+    label: "About",
+    url: "/about",
+  },
+
+  {
+    label: "Wiki",
+    url: env.NEXT_PUBLIC_WIKI_URL,
+  },
+  {
+    label: "Studio",
+    url: env.NEXT_PUBLIC_STUDIO_URL,
+  },
+];
 
 export default function Header() {
-  const links = [
-    { label: "Home", href: "/" },
-    { label: "About", href: "/about" },
-    { label: "Contact", href: "/contact" },
-    { label: "Wiki", href: process.env.NEXT_PUBLIC_WIKI_URL! },
-  ];
-
   return (
-    <header className="container sticky top-0 mx-auto w-full font-poppins text-white">
-      <nav className="flex h-14 items-center">
-        <Link href={"/public"}>Selenite</Link>
-        <ul className="flex flex-1 items-center justify-end">
-          {links.map((link) => {
-            const Element = link.href.startsWith("http") ? "a" : Link;
-
-            return (
-              <li key={link.href} className="ml-4">
-                <Element href={link.href}>{link.label}</Element>
-              </li>
-            );
-          })}
+    <header className="sticky top-0 z-50 flex justify-start px-8 pt-4">
+      <div className="flex flex-1 flex-col items-start">
+        <div className="flex w-full">
+          <div className="h-px flex-1 bg-gray-400"></div>
+          <div className="h-px w-10 flex-none origin-[0_0] rotate-45 bg-gray-400"></div>
+        </div>
+        <div className="relative top-4 pl-4">
+          <span className="font-orbitron inline-block text-4xl uppercase leading-none">Selenite</span>
+        </div>
+      </div>
+      <nav>
+        <ul className="-mt-1.5 flex gap-4 px-2 font-poppins">
+          {links.map((link) => (
+            <li key={link.url}>
+              <Link href={link.url} className="px-2 text-lg leading-none">
+                {link.label}
+              </Link>
+            </li>
+          ))}
         </ul>
+        <div className="relative top-2.5 -mx-3 h-px bg-gray-400"></div>
       </nav>
+      <div className="flex flex-1 flex-col items-end">
+        <div className="flex w-full">
+          <div className="h-px w-10 flex-none origin-[100%_0] -rotate-45 bg-gray-400"></div>
+          <div className="h-px flex-1 bg-gray-400"></div>
+        </div>
+        <div className="relative top-4 pr-4">
+          <span className="font-poppins">
+            <Link
+              href={env.NEXT_PUBLIC_DOWNLOAD_URL}
+              className={cn("text-black", buttonVariants({ variant: "outline" }))}
+            >
+              Download
+            </Link>
+          </span>
+        </div>
+      </div>
     </header>
   );
 }
