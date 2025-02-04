db571ce9529c03566f30879fa96b4bff4b609284
Pierre Guéroult <pierre.gueroult@univ-rouen.fr>
2025-02-04 18:47:33 +0100

    feat(themes): add next-themes to theme the wiki website

--- src/app/wiki/layout.tsx
+++ src/app/wiki/layout.tsx
@@ -2,14 +2,17 @@ import { ReactNode } from "react";
 
 import { SidebarProvider } from "@/components/_ui/sidebar";
 import AppSidebar from "@/components/wiki/sidebar/sidebar";
+import ThemeProvider from "@/components/wiki/theme-provider/theme-provider";
 
 type WikiLayoutProps = Readonly<{ children: ReactNode }>;
 
 export default function Layout({ children }: WikiLayoutProps): ReactNode {
   return (
     <SidebarProvider>
-      <AppSidebar />
-      <main className="w-full">{children}</main>
+      <ThemeProvider>
+        <AppSidebar />
+        <main className="w-full">{children}</main>
+      </ThemeProvider>
     </SidebarProvider>
   );
 }
