4da06a8b66d313f14a50c957da34723a9f065800
Pierre Guéroult <pierre.gueroult@univ-rouen.fr>
2025-02-05 08:12:18 +0100

    fix(wiki): prevent sidebar logo to render on the server

--- src/components/wiki/logo/logo.tsx
+++ src/components/wiki/logo/logo.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import type { ComponentProps, ReactNode } from "react";
+import { type ComponentProps, type ReactNode, useEffect, useState } from "react";
 
 import { useTheme } from "next-themes";
 
@@ -12,7 +12,17 @@ const light: [string, string] = ["#CDCDCD", "#BDBDBD"];
 const dark: [string, string] = ["#666666", "#888888"];
 
 export default function Logo(props: LogoProps): ReactNode {
+  const [mounted, setMounted] = useState(false);
   const { theme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    // workaround for next-themes to not render the logo on the server
+    return null;
+  }
 
   return (
     <DefaultLogo
