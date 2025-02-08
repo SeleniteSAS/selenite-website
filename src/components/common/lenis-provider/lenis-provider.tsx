87f7cbcf998cd82ca1a53daca0821336bca33e80
Clément Omnès <clement.mns.pro@gmail.com>
2025-02-08 12:56:02 +0100

    fix(lenis): add scrollTo effect on pathname change

--- src/components/common/lenis-provider/lenis-provider.tsx
+++ src/components/common/lenis-provider/lenis-provider.tsx
@@ -1,12 +1,24 @@
 "use client";
 
-import ReactLenis from "lenis/react";
-import type { ReactNode } from "react";
+import ReactLenis, { useLenis } from "lenis/react";
+import { type ReactNode, useEffect } from "react";
+
+import { usePathname } from "next/navigation";
+
+import Lenis from "lenis";
 
 type LenisProviderProps = Readonly<{
   children: ReactNode;
 }>;
 
 export default function LenisProvider({ children }: LenisProviderProps): ReactNode {
+  const pathname: string = usePathname();
+  const lenis: Lenis | undefined = useLenis();
+
+  useEffect(() => {
+    if (!lenis) return;
+    lenis.scrollTo(0, { immediate: true, force: true });
+  }, [pathname, lenis]);
+
   return <ReactLenis root>{children}</ReactLenis>;
 }
