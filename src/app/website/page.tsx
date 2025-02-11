c2760110da4298d3e55a61d2db574fa3efd3967d
Pierre Guéroult <pierre.gueroult@univ-rouen.fr>
2025-02-11 18:53:08 +0100

    feat(website): add Arrows component for keyboard navigation and restructure layout

--- src/app/website/page.tsx
+++ src/app/website/page.tsx
@@ -7,46 +7,57 @@ import Clock from "@/components/website/clock/clock";
 import SpaceShip from "@/components/website/spaceship/spaceship";
 
 import { env } from "@/lib/env";
+import Arrows from "@/components/website/arrows/arrows";
 
 const Moon = lazy(() => import("@/components/website/moon/moon"));
 
 export default async function WebsitePage(): Promise<ReactNode> {
   return (
     <div className="mt-32 h-[400vh]">
-      {/* <Loader /> */}
-      <div className="h-[300vh]">
-        <div className="ml-16 flex items-center gap-2">
-          <svg width="29" height="13" viewBox="0 0 233 100" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path opacity="0.8" d="M0.709961 0H47.1399L140 100H93.5701L0.709961 0Z" fill="#FFF" />
-            <path opacity="0.8" d="M93.5701 0H140L232.86 100H186.43L93.5701 0Z" fill="#FFF" />
-          </svg>
-          <p className="font-poppins font-bold uppercase">The adventures starts in </p>
-        </div>
-        <Clock />
-        <div className="mx-16 mt-4 flex items-start justify-between font-poppins">
-          <div className="text-md flex flex-col font-bold uppercase opacity-55">
-            <span className="italic">Elbeuf</span>
-            <div className="flex items-center gap-2 text-lg">
-              <svg width="20" height="20" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M25.75 24.25V0H24.25V24.25H0V25.75H24.25V50H25.75V25.75H50V24.25H25.75Z" fill="#FFF" />
-              </svg>
+      <div className="h-[300vh] flex flex-col">
+        <div className="h-[calc(100vh-8rem)]">
+          <div className="ml-16 flex items-center gap-2">
+            <svg width="29" height="13" viewBox="0 0 233 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path opacity="0.8" d="M0.709961 0H47.1399L140 100H93.5701L0.709961 0Z" fill="#FFF" />
+              <path opacity="0.8" d="M93.5701 0H140L232.86 100H186.43L93.5701 0Z" fill="#FFF" />
+            </svg>
+            <p className="font-poppins font-bold uppercase">The adventures starts in </p>
+          </div>
+          <Clock />
+          <div className="mx-16 mt-4 flex items-start justify-between font-poppins">
+            <div className="text-md flex flex-col font-bold uppercase opacity-55">
+              <span className="italic">Elbeuf</span>
+              <div className="flex items-center gap-2 text-lg">
+                <svg width="20" height="20" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M25.75 24.25V0H24.25V24.25H0V25.75H24.25V50H25.75V25.75H50V24.25H25.75Z" fill="#FFF" />
+                </svg>
 
-              <span>49° 17&apos; 14.298&apos;&apos; N</span>
-              <span className="scale-y-50">/</span>
-              <span>1° 0&apos; 32.6736&apos;&apos; E</span>
+                <span>49° 17&apos; 14.298&apos;&apos; N</span>
+                <span className="scale-y-50">/</span>
+                <span>1° 0&apos; 32.6736&apos;&apos; E</span>
+              </div>
+            </div>
+            <div className="text-md flex flex-col items-end font-bold uppercase opacity-55">
+              <span className="italic">&copy; 2025 Selenite</span>
+              <div className="flex gap-2">
+                <span>Unity Technologies</span>
+                <svg width="20" height="20" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M0 0V50H50V0H0ZM25 25L1.46002 48.54V36.41L36.41 1.41003H48.54L25 25ZM48.54 13.59V25.73L25.73 48.54H13.59L48.54 13.59ZM12.14 1.45996H24.27L1.46002 24.27V12.14L12.14 1.45996ZM37.86 48.54L48.54 37.86V48.54H37.86Z"
+                    fill="white"
+                  />
+                </svg>
+              </div>
             </div>
           </div>
-          <div className="text-md flex flex-col items-end font-bold uppercase opacity-55">
-            <span className="italic">&copy; 2025 Selenite</span>
-            <div className="flex gap-2">
-              <span>Unity Technologies</span>
-              <svg width="20" height="20" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M0 0V50H50V0H0ZM25 25L1.46002 48.54V36.41L36.41 1.41003H48.54L25 25ZM48.54 13.59V25.73L25.73 48.54H13.59L48.54 13.59ZM12.14 1.45996H24.27L1.46002 24.27V12.14L12.14 1.45996ZM37.86 48.54L48.54 37.86V48.54H37.86Z"
-                  fill="white"
-                />
-              </svg>
-            </div>
+        </div>
+        <div className="h-screen">Section 2</div>
+        <div className="flex-1 relative">
+          <div className="absolute bottom-2 right-2 -translate-x-1/2 space-y-4">
+           <Arrows />
+           <p className="text-wrap font-poppins text-sm text-center">
+            Use the arrow keys <br/>to move the spaceship
+           </p>
           </div>
         </div>
         <div className="fixed inset-0 -z-10 h-screen w-screen">
@@ -56,7 +67,7 @@ export default async function WebsitePage(): Promise<ReactNode> {
           </Canvas>
         </div>
       </div>
-      <div className="h-screen"></div>
+      <div className="h-screen text-white">FOOTER</div>
       <div className="fixed bottom-8 left-8 font-orbitron text-lg">
         DEVELOPED BY
         <br />
